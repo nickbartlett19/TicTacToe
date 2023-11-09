a22263419109af58b810307af3bc9e33b5cb7e27
Nick Bartlett <nickbartlett19@gmail.com>
2023-11-09 13:36:50 -0500

    set up TicTacToe class

--- script.js
+++ script.js
@@ -1,21 +1,49 @@
+class TicTacToe {
+    constructor(choice = "default") {
+        this.p1choice = choice;
+    }
+
+    newGame() {
+
+    }
+
+    winCheck() {
+
+    }
+
+    placeMove(gameTile) {
+        gameTile.innerText = this.p1choice;
+        // console.log(gameTiles);
+        // gameTiles.innerHTML = this.p1choice;
+    }
+
+    choose(piece = "default") {
+        this.p1Choice = piece;
+        chooseBtn.style.display = "none";
+    }
+
+}
 const chooseBtn = document.querySelector(".choose-button");
 const xBtn = document.querySelector(".x-button");
 const oBtn = document.querySelector(".o-button");
+const gameTiles = document.querySelectorAll(".game-tile");
 
-
-var p1Choice;
+const myGame = new TicTacToe("x");
 
 xBtn.addEventListener('click', (e) => {
-    p1Choice = "x";
-    chooseBtn.style.display = "none";
-    console.log(p1Choice);
+    myGame.choose("x");
 });
 
 oBtn.addEventListener('click', (e) => {
-    p1Choice = "o";
-    chooseBtn.style.display = "none";
-    console.log(p1Choice);
+    myGame.choose("o");
 });
 
+gameTiles.forEach(button => {
+    button.addEventListener('click', () => {
+        myGame.placeMove(button);
+    })
+})
 
 
+
+// console.log(myGame.p1choice);
