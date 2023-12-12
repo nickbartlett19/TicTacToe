96437957384a5cd829150220edbe68efa61f1d67
Nick Bartlett <nickbartlett19@gmail.com>
2023-12-12 13:54:11 -0500

    add comments, clean code

--- script.js
+++ script.js
@@ -1,28 +1,52 @@
+// _____ _     _____         _____          
+// |_   _(_) __|_   _|_ _  __|_   _|__   ___ 
+//   | | | |/ __|| |/ _` |/ __|| |/ _ \ / _ \
+//   | | | | (__ | | (_| | (__ | | (_) |  __/
+//   |_| |_|\___||_|\__,_|\___||_|\___/ \___|
+
+// TicTacToe Game Rules:
+
+// - Player designated to go first (p1) selects their desired piece (X, or O)
+// - They select a square to place their piece
+// - Player 2 (p2) selects a square to place their piece
+// - This continues until a player has connected three of their pieces diagonally, vertically
+//   or horizontally, or, until all squares are full, which results in a draw
+
+// Additional features of this program:
+
+// - 
+
+// How this program is structured: 
+
+// - TicTacToe object is defined 
+
+
 class TicTacToe {
-    constructor(choice = "default") {
-        this.p1choice = choice;
+    constructor() {
+        this.choice;
     }
 
     newGame() {
-
+        // code goes here
     }
 
     winCheck() {
-
+        // code goes here
     }
 
     placeMove(gameTile) {
-        gameTile.innerText = this.p1choice;
+        gameTile.innerText = this.choice;
         // console.log(gameTiles);
         // gameTiles.innerHTML = this.p1choice;
     }
 
-    choose(piece = "default") {
-        this.p1Choice = piece;
+    choose(piece) {
+        this.choice = piece;
         chooseBtn.style.display = "none";
     }
 
 }
+
 const chooseBtn = document.querySelector(".choose-button");
 const xBtn = document.querySelector(".x-button");
 const oBtn = document.querySelector(".o-button");
@@ -43,7 +67,3 @@ gameTiles.forEach(button => {
         myGame.placeMove(button);
     })
 })
-
-
-
-// console.log(myGame.p1choice);
