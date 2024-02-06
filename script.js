992698aae35cd8754bdf50e22590fa399a835169
Nick Bartlett <nickbartlett19@gmail.com>
2024-02-06 13:33:32 -0500

    debug clearBoard()

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// _____ _     _____         _____          
-// |_   _(_) __|_   _|_ _  __|_   _|__   ___ 
-//   | | | |/ __|| |/ _` |/ __|| |/ _ \ / _ \
-//   | | | | (__ | | (_| | (__ | | (_) |  __/
-//   |_| |_|\___||_|\__,_|\___||_|\___/ \___|
-
-// TicTacToe Game Rules:
-
-// - Player designated to go first (p1) selects their desired piece (X, or O)
-// - They select a square to place their piece
-// - Player 2 (p2) selects a square to place their piece
-// - This continues until a player has connected three of their pieces diagonally, vertically
-//   or horizontally, or, until all squares are full, which results in a draw
-
-// Additional features of this program:
-
-// - tracks score (wins, losses, draws of either player) (to be implemented)
-
-// How this program is designed: 
-
-// - Game functions are defined in the TicTacToe object
-// - board is represented by a 2d array, a magic square is then used to track if a player has
-// won based on the property that any row, column, or diagonal sums to the same number
-
 class TicTacToe {
     constructor() {
         this.choice;
@@ -182,6 +158,18 @@ class TicTacToe {
                 gameTiles[i].removeChild(gameTiles[i].firstChild);
             }
         }
+
+        // reset boards
+        this.xBoard = [
+            [0,0,0],
+            [0,0,0],
+            [0,0,0]
+        ];
+        this.oBoard = [
+            [0,0,0],
+            [0,0,0],
+            [0,0,0]
+        ];
     }
 
     updateMagicSquare(id, selector) {
@@ -218,26 +206,6 @@ class TicTacToe {
     }
 
 }
-
-// var xBoard = math.matrix([
-//     [1,0,0],
-//     [0,0,0],
-//     [0,0,0]
-// ]);
-
-// console.log(typeof(xBoard)); 
-// console.log(xBoard[0,0]);
-
-// var xBoard = [
-//     [1,2,3],
-//     [0,0,0],
-//     [0,0,0]
-// ];
-
-// console.log(typeof(xBoard)); 
-// console.log(xBoard[0][0]);
-// console.log(xBoard[1][0]);
-// console.log(xBoard[2][0]);
 
 const chooseBtn = document.querySelector(".choose-button");
 const xBtn = document.querySelector(".x-button");
